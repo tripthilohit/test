2517d6367039dc0285ce3c2504f6218e10a8032c
sukadar <sukada.ramakrishna@socialmedialink.com>
2018-02-07 16:42:26 -0500

    dar changes

--- tests/globals.js
+++ tests/globals.js
@@ -1,5 +1,5 @@
 var userNames = {
-  memberURL: 'https://tripsprint95t.staging.socialmedialink.com',
+  memberURL: 'https://sul.staging.socialmedialink.com/',
   memberEmail: 'mark@yopmail.com',
   memberPassword: '12345678',
   memberFirstName: 'first',
@@ -8,18 +8,18 @@ var userNames = {
   birthDay: '10/10/1991',
   sprint: '95t',
   adminURL: 'https://admin.staging.socialmedialink.com',
-  adminEmail: 'tripsprint95t@yopmail.com',
+  adminEmail: 'suli@yopmail.com',
   adminPassword: 'General123$',
-  facebookEmail: 'kqqdfbv_alisonsen_1454598705@tfbnw.net',
+  facebookEmail: 'sukada.testmember8@socialmedialink.com',
   facebookPass: 'general1234',
-  twitterEmail: 'tripthi.testmember15@socialmedialink.com',
+  twitterEmail: 'sukada.ramakrishna@socialmedialink.com',
   twitterPass: 'general1234',
   instagramUserName: 'tripthi.shetty',
   instagramPass: 'abcd1234',
   superAdminEmail: 'tripthi.testmember3@socialmedialink.com',
   superAdminPass: 'General123$',
   account_no: 666,
-  path: '/Users/tripthi/Documents/images/scriptImages/'
+  path: '/Users/Sukad/Documents/images/scriptImages/'
 };
 
 var cbt = require('cbt_tunnels');
@@ -28,8 +28,8 @@ module.exports = {
   beforeEach : function(done) {
     console.log('Starting up tunnel');
     cbt.start({
-      'username': 'tripthi.shetty@socialmedialink.com',
-      'authkey': 'u283c7d7d4fafeb7'
+      'username': 'sukada.ramakrishna@socialmedialink.com',
+      'authkey': 'u89e4efd851e17e7'
     }, function(err, data) {
       if (err) {
         done(err);
@@ -39,7 +39,7 @@ module.exports = {
     });
   },
   userNames: userNames,
-  
+
   afterEach : function(done) {
     console.log('Closing Down Tunnel');
     cbt.stop();
